166250152282b38aa3121d57450a6f315932836d
sushantkapare-dev <kapare.sushant23@gmail,com>
2026-01-09 19:39:15 +0530

    Added map files

---
+++ 1_Golang_Basics/16_range_over_slice/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	views := []int{10, 20, 45, 50, 60}
+
+	// for range
+	total := 0
+	for i, v := range views {
+		fmt.Println("day", i, "views", v)
+		total = total + v
+	}
+
+	fmt.Println(total)
+}
