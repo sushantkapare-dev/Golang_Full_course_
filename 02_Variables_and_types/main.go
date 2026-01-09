337b5568e46c17b3a12778ef3858787039057adb
sushantkapare-dev <kapare.sushant23@gmail,com>
2026-01-09 09:28:11 +0530

    Initial commit

---
+++ 02_Variables_and_types/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var channelName string
+	channelName = "TrainwithSushant"
+
+	var year int = 2026
+
+	fmt.Println("Channel", channelName)
+	fmt.Println("year:", year)
+
+	var rating float64 = 4.8
+	fmt.Println("Rating:", rating)
+
+}
